f5803723972955eae7e2d8dca5d5166b65ab5eee
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2020-10-16 05:16:06 -0300

    Create checkbox_and_radio.tsx

---
+++ widgets/checkbox_and_radio.tsx
@@ -0,0 +1,25 @@
+<Checkbox>
+  <CheckboxChecked sx={{ }}>
+    {content}
+  </CheckboxChecked>
+
+  <CheckboxUnChecked sx={{ }}>
+    {context}
+  </CheckboxUnChecked>
+
+  {content}
+</Checkbox>
+
+-------------------------------
+
+<Radio>
+  <RadioChecked sx={{ }}>
+    {content}
+  </RadioChecked>
+
+  <RadioUnChecked sx={{ }}>
+    {context}
+  </RadioUnChecked>
+
+  {content}
+</Radio>
