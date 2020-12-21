03e6294a50e414ad606d20d911befca095528993
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2020-12-21 06:44:05 -0300

    Update screenDimensions.ts

--- hooks/screenDimensions.ts
+++ hooks/screenDimensions.ts
@@ -4,8 +4,8 @@ export default function screenDimensions() {
   const [screenDimensions, setScreenDimensions] = useState<{ width: number, height: number }>({ width: undefined, height: undefined });
 
   useEffect(() => {
-    setScreenDimensions({ width: window.innerWidth, height: window.innerHeight });
     function handleResize() { setScreenDimensions({ width: window.innerWidth, height: window.innerHeight }); }
+    handleResize();
 
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
